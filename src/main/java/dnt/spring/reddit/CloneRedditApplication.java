31b642699538965174397e89be6507ae1f9d3db2
dangngoctam00 <dangngoctam00@gmail.com>
2021-03-22 14:15:08 +0700

    swagger ui

--- src/main/java/dnt/spring/reddit/CloneRedditApplication.java
+++ src/main/java/dnt/spring/reddit/CloneRedditApplication.java
@@ -2,10 +2,14 @@ package dnt.spring.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import dnt.spring.reddit.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class CloneRedditApplication {
 
 	public static void main(String[] args) {
